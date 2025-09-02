1d2a693a78e2d9ab2b41fbea1950c9d9f4dd0bb1
dibadull <dinmukhammed.ibadulla@nu.edu.kz>
2025-09-02 22:50:50 +0500

    added handler, route and helpers

---
+++ cmd/web/main.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	"database/sql"
+	"flag"
+	"log"
+	"net/http"
+	"os"
+)
+
+type application struct {
+	errorLog *log.Logger
+	infoLog  *log.Logger
+}
+
+func openDB(dsn string) (*sql.DB, error) {
+	db, err := sql.Open("mysql", dsn)
+	if err != nil {
+		return nil, err
+	}
+	if err = db.Ping(); err != nil {
+		return nil, err
+	}
+
+	return db, nil
+}
+
+func main() {
+	addr := flag.String("addr", ":4000", "HTTP network address")
+	dsn := flag.String("dsn", "web:Soo577dis@/snippetbox?parseTime=true", "MySQL database")
+	flag.Parse()
+
+	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
+	errorLog := log.New(os.Stdout, "ERROR\t", log.Ldate|log.Ltime)
+
+	app := &application{
+		errorLog: errorLog,
+		infoLog:  infoLog,
+	}
+
+	srv := &http.Server{
+		Addr:     *addr,
+		ErrorLog: errorLog,
+		Handler:  app.routes(),
+	}
+
+	db, err := openDB(*dsn)
+	if err != nil {
+		errorLog.Fatal(err)
+		return
+	}
+
+	defer db.Close()
+
+	infoLog.Printf("Starting server on %s", *addr)
+	err = srv.ListenAndServe()
+	if err != nil {
+		errorLog.Fatal(err)
+	}
+}
