00cefb8e382465fe37f9353dc35002a371b8eae9
dibadull <dinmukhammed.ibadulla@nu.edu.kz>
2025-09-15 15:42:04 +0500

    catching runtime errors

--- cmd/web/helpers.go
+++ cmd/web/helpers.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
 	"net/http"
 	"runtime/debug"
@@ -29,9 +30,14 @@ func (app *application) render(w http.ResponseWriter, r *http.Request, name stri
 		app.serverError(w, fmt.Errorf("The template %s does not exist", name))
 		return
 	}
-	// Execute the template set, passing in any dynamic data.
-	err := ts.Execute(w, td)
+
+	buf := new(bytes.Buffer)
+
+	err := ts.Execute(buf, td)
 	if err != nil {
 		app.serverError(w, err)
+		return
 	}
+
+	buf.WriteTo(w)
 }
